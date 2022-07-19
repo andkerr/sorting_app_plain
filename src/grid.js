1e749ec6dc4a5edbb7ffcf8bf37b02432019c29f
Andrew Kerr <kerrand@protonmail.com>
2022-07-19 19:26:11 -0400

    update Grid class

--- src/grid.js
+++ src/grid.js
@@ -1,161 +1,77 @@
 class Grid {
+  #canvasElt;
+  #cellWidth;
+  #cellHeight;
+  #gridWidth;
+  #gridHeight;
+  #gutter;
+  #nCols;
+  #nRows;
+
   constructor(canvasEltId, cellWidth, cellHeight, gutter) {
-    this.canvasElt = document.getElementById(canvasEltId);
-    this.cellWidth = cellWidth;
-    this.cellHeight = cellHeight;
-    this.gutter = gutter;
+    this.#canvasElt = document.getElementById(canvasEltId);
+    this.#cellWidth = cellWidth;
+    this.#cellHeight = cellHeight;
+    this.#gutter = gutter;
 
-    this.nCols = this.canvasElt.width / this.cellWidth;
-    this.nRows = this.canvasElt.height / this.cellHeight;
+    this.#nCols = Math.floor((this.#canvasElt.width - this.#gutter) / (this.#cellWidth + this.#gutter));
+    this.#nRows = Math.floor((this.#canvasElt.height - this.#gutter) / (this.#cellHeight + this.#gutter));
+  
+    this.#gridWidth = this.#nCols * (this.#cellWidth + this.#gutter) + this.#gutter;
+    this.#gridHeight = this.#nRows * (this.#cellHeight + this.#gutter) + this.#gutter;
+
+    let extraX = this.#canvasElt.width - this.#gridWidth;
+    let extraY = this.#canvasElt.height - this.#gridHeight;
+    console.log(`extraX: ${extraX}, extraY: ${extraY}`);
+    
+    // center the grid in the excess horizontal and vertical space
+    let context = this.#canvasElt.getContext('2d');
+    context.translate(Math.floor(extraX / 2), Math.floor(extraY / 2));
+  
+    this.clearGrid(); // to start, render an empty grid
   }
 
-  draw(data) {
-    let context = this.canvasElt.getContext('2d');
+  get nCols() {
+    return this.#nCols;
+  }
+
+  get nRows() {
+    return this.#nRows;
+  }
+
+  clearGrid() {
+    let context = this.#canvasElt.getContext('2d');
 
     context.fillStyle = '#cccccc';
-    context.fillRect(0, 0, this.canvasElt.width, this.canvasElt.height);
+    context.fillRect(0, 0, this.#gridWidth, this.#gridHeight);
 
     context.fillStyle = '#ffffff';
-    for (let i = 0; i < this.nCols; ++i) {
-      for (let j = 0; j < this.nRows; ++j) {
+    for (let i = 0; i < this.#nCols; ++i) {
+      for (let j = 0; j < this.#nRows; ++j) {
         context.save();
 
-        context.translate(j * this.cellHeight, i * this.cellWidth);
-        context.fillRect(this.gutter,
-                         this.gutter,
-                         this.cellWidth - this.gutter,
-                         this.cellHeight - this.gutter);
+        context.translate(i * (this.#cellWidth + this.#gutter) + this.#gutter,
+                          j * (this.#cellHeight + this.#gutter) + this.#gutter);
+        context.fillRect(0, 0, this.#cellWidth, this.#cellHeight);
 
         context.restore();
       }
     }
   }
 
-  clear() {
-    let context = this.canvasElt.getContext('2d');
+  fillCell(i, j, color = '#cc8800') {
+    let context = this.#canvasElt.getContext('2d');
+    context.fillStyle = color;
+    context.save();
 
-    context.fillStyle = '#cccccc';
-    context.fillRect(0, 0, this.canvasElt.width, this.canvasElt.height);
+    context.translate(i * (this.#cellWidth + this.#gutter) + this.#gutter,
+                      j * (this.#cellHeight + this.#gutter) + this.#gutter);
+    context.fillRect(0, 0, this.#cellWidth, this.#cellHeight);
 
-    context.fillStyle = '#ffffff';
-    for (let i = 0; i < this.cellWidth; ++i) {
-      for (let j = 0; j < this.cellHeight; ++j) {
-        context.save();
+    context.restore();
+  }
 
-        context.translate(j * this.cellHeight + this.gutter,
-                          i * this.cellWidth + this.gutter);
-        context.fillRect(0, 0, this.cellWidth - this.gutter,
-                         this.cellHeight - this.gutter);
-
-        context.restore();
-      }
-    }
+  clearCell(i, j) {
+    this.fillCell(i, j, '#ffffff');
   }
 }
-
-// (function(global) {
-//   const ROWS = 32,
-//         COLS = 32;
-// 
-//   var canvas = document.getElementById("canvas"),
-//       context = canvas.getContext("2d");
-// 
-//   var width = Math.min(global.innerWidth, global.innerHeight) * 0.9;
-//   var height = width;
-// 
-//   cellWidth = Math.floor(width / COLS),
-//   cellHeight = Math.floor(height / ROWS);
-// 
-//   gutter = Math.ceil(0.1 * cellWidth);
-// 
-//   canvas.width = cellWidth * COLS + gutter;
-//   canvas.height = cellHeight * ROWS + gutter;
-// 
-//   context.fillStyle = "#cccccc"; // grey background
-//   context.fillRect(0, 0, canvas.width, canvas.height);
-//   context.fillStyle = "#ffffff"; // default cell fill
-// 
-//   for (var i = 0; i < ROWS; ++i) {
-//     for (var j = 0; j < COLS; ++j) {
-//       context.save();
-//       context.translate(j * cellWidth, i * cellHeight);
-// 
-//       // drawing code
-//       context.fillRect(gutter, gutter, cellWidth - gutter, cellHeight - gutter);
-// 
-//       context.restore();
-//     }
-//   }
-// })(window);
-// 
-// initialize a <canvas> element that displays an n x n grid
-// export function gridInit(g) {
-//   var canvas = document.getElementById("canvas"),
-//       context = canvas.getContext("2d");
-//
-//   var width = Math.min(window.innerWidth, window.innerHeight) * 0.9;
-//   var height = width;
-//
-//   g.cellWidth = Math.floor(width / g.cols),
-//   g.cellHeight = Math.floor(height / g.rows);
-//
-//   g.gutter = Math.ceil(0.1 * g.cellWidth);
-//
-//   canvas.width = g.cellWidth * g.cols + g.gutter;
-//   canvas.height = g.cellHeight * g.rows + g.gutter;
-//
-//   context.fillStyle = "#cccccc"; // grey background
-//   context.fillRect(0, 0, canvas.width, canvas.height);
-//   context.fillStyle = "#ffffff"; // default cell fill
-//
-//   for (var i = 0; i < g.rows; ++i) {
-//     for (var j = 0; j < g.cols; ++j) {
-//       context.save();
-//       context.translate(j * g.cellWidth, i * g.cellHeight);
-//
-//       // drawing code
-//       context.fillRect(g.gutter, g.gutter, g.cellWidth - g.gutter, g.cellHeight - g.gutter);
-//
-//       context.restore();
-//     }
-//   }
-//
-//   return g;
-// }
-//
-// function gridClear(g) {
-//   var canvas = document.getElementById("canvas"),
-//       context = canvas.getContext("2d");
-//
-//   context.fillStyle = "#ffffff"; // default cell fill
-//
-//   for (var i = 0; i < g.rows; ++i) {
-//     for (var j = 0; j < g.cols; ++j) {
-//       context.save();
-//       context.translate(j * g.cellWidth, i * g.cellHeight);
-//
-//       // drawing code
-//       context.fillRect(g.gutter, g.gutter, g.cellWidth - g.gutter, g.cellHeight - g.gutter);
-//
-//       context.restore();
-//     }
-//   }
-// }
-//
-// export function drawCells(g, vals) {
-//   gridClear(g);
-//
-//   var canvas = document.getElementById("canvas"),
-//       context = canvas.getContext("2d");
-//
-//   context.fillStyle = "#cc8800";
-//
-//   for (let i = 0; i < vals.length; ++i) {
-//     context.save();
-//     context.translate(i * g.cellWidth, (g.rows - vals[i] - 1) * g.cellHeight);
-//
-//     context.fillRect(g.gutter, g.gutter, g.cellWidth - g.gutter, g.cellHeight - g.gutter);
-//
-//     context.restore();
-//   }
-// }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 function main() {
-    let grid = new Grid('canvas', 20, 20, 3);
-    grid.draw();
+    let grid = new Grid('canvas', 30, 30, 4);
+    let data = [...Array(grid.nCols).keys()];
+    for (let i = 0; i < data.length; ++i) {
+        grid.fillCell(i, data[i]);
+    }
 }
 
 document.addEventListener('DOMContentLoaded', main);
